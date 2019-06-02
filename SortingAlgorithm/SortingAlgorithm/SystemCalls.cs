039ce86187396c3894b1932ce073d6ae82c1eb88
申静轩 <shenjingxuan1993@gmail.com>
2019-06-03 01:50:43 +0800

    建立项目，main方法，调用方法处

---
+++ SortingAlgorithm/SortingAlgorithm/SystemCalls.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SortingAlgorithm
+{
+    class SortingAligorithm
+    {
+        static void Main(String[] arg0)
+        {
+            /* 排序方法调用处 */
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
